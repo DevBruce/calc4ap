6aba0ca35d79216ccf1dc74accb0acbf194cc29a
DevBruce <bruce93k@gmail.com>
2021-06-14 11:22:47 +0900

    Encapsulate codes

--- calc4ap/libs/calc_ap.py
+++ calc4ap/libs/calc_ap.py
@@ -6,36 +6,52 @@ __all__ = ['CalcAP']
 
 class CalcAP:
     def __init__(self, labels, preds, iou_thr=0.5, conf_thr=0.0):
-        self.labels = labels
-        self.preds = preds
-        self.iou_thr = iou_thr
-        self.conf_thr = conf_thr
-        self.precisions = dict()
-        self.recalls = dict()
+        self._labels = labels
+        self._preds = preds
+        self._iou_thr = iou_thr
+        self._conf_thr = conf_thr
+        self._precisions = dict()
+        self._recalls = dict()
 
-        pr_data = self._get_pr()
-        self.precisions['origin'] = pr_data['precisions']
-        self.recalls['origin'] = pr_data['recalls']
-        self.TP = pr_data['TP']
-        self.FP = pr_data['FP']
-        self.FN = len(self.labels) - self.TP
-        self.tp_avg_iou = pr_data['tp_avg_iou']
-        self.precision = self.TP / (self.TP + self.FP) if (self.TP + self.FP) else 0.0
-        self.recall = self.TP / len(self.labels)
-        if (self.precision + self.recall) == 0:
-            self.f1_score = 0.0
+        pr_data = self._calc_pr()
+        self._precisions['origin'] = pr_data['precisions']
+        self._recalls['origin'] = pr_data['recalls']
+        self._TP = pr_data['TP']
+        self._FP = pr_data['FP']
+        self._FN = len(self._labels) - self._TP
+        self._tp_avg_iou = pr_data['tp_avg_iou']
+        self._precision = self._TP / (self._TP + self._FP) if (self._TP + self._FP) else 0.0
+        self._recall = self._TP / len(self._labels)
+        if (self._precision + self._recall) == 0:
+            self._f1_score = 0.0
         else:
-            self.f1_score = 2 * (self.precision * self.recall) / (self.precision + self.recall)
+            self._f1_score = 2 * (self._precision * self._recall) / (self._precision + self._recall)
         
-        voc_ap_data = self._get_voc_ap()
-        self.AP = voc_ap_data['AP']
-        self.precisions['interpolated'] = voc_ap_data['interpolated_precisions']
-        self.recalls['interpolated'] = voc_ap_data['interpolated_recalls']
+        voc_ap_data = self._calc_voc_ap()
+        self._AP = voc_ap_data['AP']
+        self._precisions['interpolated'] = voc_ap_data['interpolated_precisions']
+        self._recalls['interpolated'] = voc_ap_data['interpolated_recalls']
+
+    def get(self):
+        ret = {
+            'n_labels': len(self._labels),
+            'TP': self._TP,
+            'FP': self._FP,
+            'FN': self._FN,
+            'tp_avg_iou': self._tp_avg_iou,
+            'precision': self._precision,
+            'recall': self._recall,
+            'f1_score': self._f1_score,
+            'precisions': self._precisions,
+            'recalls': self._recalls,
+            'AP': self._AP,
+        }
+        return ret
         
-    def _get_ious_confs_useds(self):
-        labels_mapped = self.labels.map_by_img_id_with_used()
+    def _calc_ious_confs_useds(self):
+        labels_mapped = self._labels.map_by_img_id_with_used()
         ious, confs, useds = list(), list(), list()
-        for pred in self.preds:
+        for pred in self._preds:
             *pts, conf, img_id = pred
 
             img_labels = labels_mapped.get(img_id, [])
@@ -49,7 +65,7 @@ class CalcAP:
                 max_iou = max(img_ious)
                 mapped_label_idx = img_ious.index(max_iou)
                 used = img_labels[mapped_label_idx].get('used')
-                if max_iou >= self.iou_thr and not used:
+                if max_iou >= self._iou_thr and not used:
                     img_labels[mapped_label_idx]['used'] = True
             else:
                 max_iou, used = 0.0, False
@@ -59,21 +75,21 @@ class CalcAP:
             useds.append(used)
         return ious, confs, useds
 
-    def _get_pr(self):
+    def _calc_pr(self):
         TP, FP = 0, 0
         precisions, recalls = [0.0], [0.0]
         tp_ious_sum = 0
 
-        ious, confs, useds = self._get_ious_confs_useds()
+        ious, confs, useds = self._calc_ious_confs_useds()
         for iou, conf, used in zip(ious, confs, useds):
-            if iou >= self.iou_thr and conf >= self.conf_thr and not used:
+            if iou >= self._iou_thr and conf >= self._conf_thr and not used:
                 TP += 1
                 tp_ious_sum += iou
             else:
                 FP += 1
                 
             precision = TP / (TP + FP)
-            recall = TP / len(self.labels)
+            recall = TP / len(self._labels)
             precisions.append(precision)
             recalls.append(recall)
             
@@ -90,9 +106,9 @@ class CalcAP:
         }
         return ret
 
-    def _get_voc_ap(self):
-        precisions = self.precisions['origin'].copy()
-        recalls = self.recalls['origin'].copy()
+    def _calc_voc_ap(self):
+        precisions = self._precisions['origin'].copy()
+        recalls = self._recalls['origin'].copy()
         
         # Interpolation
         for i in range(len(precisions)-2, -1, -1):
--- calc4ap/voc.py
+++ calc4ap/voc.py
@@ -7,52 +7,75 @@ __all__ = ['CalcVOCmAP']
 
 class CalcVOCmAP:
     def __init__(self, labels, preds, iou_thr=0.5, conf_thr=0.0):
-        self.labels = classify_labels(labels)
-        self.preds = classify_preds(preds)
-        self.iou_thr = iou_thr
-        self.conf_thr = conf_thr
-        self.APs = self._get_APs()
+        self._labels = classify_labels(labels)
+        self._preds = classify_preds(preds)
+        self._n_classes = len(self._labels)
+        self._iou_thr = iou_thr
+        self._conf_thr = conf_thr
+        self._APs = self._calc_APs()
 
-        mAP_data = self._get_mAP()
-        self.mAP = mAP_data['mAP']
-        self.w_mAP = mAP_data['w_mAP']
-        self.total_TP = mAP_data['total_TP']
-        self.total_FP = mAP_data['total_FP']
-        self.total_FN = mAP_data['total_FN']
-        self.m_tp_avg_iou = mAP_data['m_tp_avg_iou']
-        self.m_precision = mAP_data['m_precision']
-        self.m_recall = mAP_data['m_recall']
-        self.m_f1_score = mAP_data['m_f1_score']
+        mAP_data = self._calc_mAP()
+        self._mAP = mAP_data['mAP']
+        self._w_mAP = mAP_data['w_mAP']
+        self._total_TP = mAP_data['total_TP']
+        self._total_FP = mAP_data['total_FP']
+        self._total_FN = mAP_data['total_FN']
+        self._m_tp_avg_iou = mAP_data['m_tp_avg_iou']
+        self._m_precision = mAP_data['m_precision']
+        self._m_recall = mAP_data['m_recall']
+        self._m_f1_score = mAP_data['m_f1_score']
 
-    def _get_APs(self):
+    def get(self):
+        ret = {
+            'total_TP': self._total_TP,
+            'total_FP': self._total_FP,
+            'total_FN': self._total_FN,
+            'm_tp_avg_iou': self._m_tp_avg_iou,
+            'm_precision': self._m_precision,
+            'm_recall': self._m_recall,
+            'm_f1_score': self._m_f1_score,
+            'APs': self._APs,
+            'mAP': self._mAP,
+            'w_mAP': self._w_mAP,
+        }
+        return ret
+
+    def get_summary(self):
+        summary = dict()
+        for cls_name in self._APs:
+            summary[cls_name] = self._APs[cls_name]['AP']
+        summary['mAP'] = self._mAP
+        return summary
+
+    def _calc_APs(self):
         APs = dict()
-        for cls_name in self.labels:
+        for cls_name in self._labels:
             AP = CalcAP(
-                labels=self.labels[cls_name],
-                preds=self.preds[cls_name],
-                iou_thr=self.iou_thr,
-                conf_thr=self.conf_thr,
-            )
+                labels=self._labels[cls_name],
+                preds=self._preds[cls_name],
+                iou_thr=self._iou_thr,
+                conf_thr=self._conf_thr,
+            ).get()
             APs[cls_name] = AP
         return APs
 
-    def _get_mAP(self):
+    def _calc_mAP(self):
         APs = 0.0
         TPs, FPs, FNs = 0, 0, 0
         sum_tp_avg_iou = 0.0
-        sum_label_length = 0
-        for cls_name in self.labels:
-            APs += self.APs[cls_name].AP
-            TPs += self.APs[cls_name].TP
-            FPs += self.APs[cls_name].FP
-            FNs += self.APs[cls_name].FN
-            sum_label_length += len(self.APs[cls_name].labels)
-            sum_tp_avg_iou += self.APs[cls_name].tp_avg_iou
+        sum_n_labels = 0
+        for cls_name in self._labels:
+            APs += self._APs[cls_name]['AP']
+            TPs += self._APs[cls_name]['TP']
+            FPs += self._APs[cls_name]['FP']
+            FNs += self._APs[cls_name]['FN']
+            sum_n_labels += self._APs[cls_name]['n_labels']
+            sum_tp_avg_iou += self._APs[cls_name]['tp_avg_iou']
 
-        mAP = APs / len(self.labels)
-        m_tp_avg_iou = sum_tp_avg_iou / len(self.labels)
+        mAP = APs / self._n_classes
+        m_tp_avg_iou = sum_tp_avg_iou / self._n_classes
         m_precision = TPs / (TPs + FPs) if (TPs + FPs) else 0.0
-        m_recall = TPs / sum_label_length
+        m_recall = TPs / sum_n_labels
         if (m_precision + m_recall) == 0:
             m_f1_score = 0.0
         else:
@@ -60,10 +83,10 @@ class CalcVOCmAP:
         
         # Weighted mAP
         w_APs = list()
-        for cls_name in self.labels:
-            w_AP = self.APs[cls_name].AP * len(self.APs[cls_name].labels)
+        for cls_name in self._labels:
+            w_AP = self._APs[cls_name]['AP'] * self._APs[cls_name]['n_labels']
             w_APs.append(w_AP)
-        w_mAP = sum(w_APs) / sum_label_length
+        w_mAP = sum(w_APs) / sum_n_labels
 
         ret = {
             'mAP': mAP,
@@ -77,10 +100,3 @@ class CalcVOCmAP:
             'm_f1_score': m_f1_score,
         }
         return ret
-
-    def get_summary(self):
-        summary_data = dict()
-        for cls_name in self.APs:
-            summary_data[cls_name] = self.APs[cls_name].AP
-        summary_data['mAP'] = self.mAP
-        return summary_data
